5a0be46297ceb7223de90a51766b3eadcb9fee0c
root <soumojyotiganguly270@gmail.com>
2020-08-26 00:02:17 +0530

    store data into firestore database

--- app/src/main/java/com/arobit/chatapp/MainActivity.java
+++ app/src/main/java/com/arobit/chatapp/MainActivity.java
@@ -1,14 +1,57 @@
 package com.arobit.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    FirebaseFirestore db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            db = FirebaseFirestore.getInstance();
+
+            save();
+
+
+        } catch (Exception e) {
+            Toast.makeText(this, "1 Error: " + e, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void save(){
+        Student student = new Student("soumo","bat4865@outlook.com");
+        db.collection("Student Info").document("2").set(student)
+        .addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful())
+                    Toast.makeText(getApplicationContext(),"Data store Successful",Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(getApplicationContext(),"Data store Failed: "+ task.getException().getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
